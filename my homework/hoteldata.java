043542971fd6362012583eec30972e548e47beb0
Bahar Razavi <s.bahar.razavi@gmail.com>
2022-12-16 17:01:58 +0330

    init

---
+++ my homework/hoteldata.java
@@ -0,0 +1,21 @@
+public class hoteldata{
+    public static void main(String[] args) { // method
+        /*
+        * This program is a summary of the lessons.
+        *This datas are about the admission in a hotel.
+        */
+        
+        //How to make a data variable--> type name = value;
+        String gustName="Bahar";           
+        String gustLastname="Razavi";      
+        float moneyAmount=47.5f;            
+        short yearOfBirth=2008 ,thisYear = 2022; 
+        boolean isFemale=true;             
+        char firstLetterName='B';
+        String nationality="Iranian";
+        // print
+        System.out.println("Her name is "+gustName+" "+gustLastname);
+        System.out.print("She is "+(thisYear-yearOfBirth));
+        System.out.println(" years old.\nShe is \"Iranian\".");
+    }
+}
